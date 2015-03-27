0ac047928d0b62678f6cd6aa66cc9532c55efec7
Scott Lundgren <scott.lundgrne@carbonblack.com>
2015-03-27 16:56:25 -0700

    CBAPI-11, CBAPI-16: add small icon and category support for the raw feed example

--- example/raw/generate_feed_from_raw_iocs.py
+++ example/raw/generate_feed_from_raw_iocs.py
@@ -116,7 +116,19 @@ def create_feed(options):
     if options.icon:
         bytes = base64.b64encode(open(options.icon).read())
         feedinfo['icon'] = bytes 
-        
+    
+    # if a small icon was provided, encode as base64 and 
+    # include in the feed information
+    #
+    if options.small_icon:
+        bytes = base64.b64encode(open(options.small_icon).read())
+        feedinfo['icon_small']
+  
+    # if a feed category was provided, include it in the feed information
+    #
+    if options.category:
+        feedinfo['category'] = options.category
+ 
     # build a CbFeedInfo instance
     # this does field validation
     #    
@@ -150,8 +162,12 @@ def _build_cli_parser():
                       help="Feed Summary")
     parser.add_option("-t", "--techdata", action="store", type="string", dest="techdata",
                       help="Feed Technical Description")
+    parser.add_option("-c", "--category", action="store", type="string", dest="category",
+                      help="Feed Category")
     parser.add_option("-i", "--icon", action="store", type="string", dest="icon",
                       help="Icon File (PNG format)")
+    parser.add_option("-S", "--small-icon", action="store", type="string", dest="small_icon",
+                      help="Small icon file (50x50 pixels) (PNG format)")
     parser.add_option("-I", "--iocs", action="store", type="string", dest="ioc_filename",
                       help="IOC filename")
     parser.add_option("-r", "--report", action="store", type="string", dest="report",
